0caf0e04584c707f454bcbf5dab32339ee362d6b
Ryan Pisuena <Pisuenaryan@gmail.com>
2018-08-16 22:52:36 -0700

    Functioning STEP 7

--- 2D_Game/script.js
+++ 2D_Game/script.js
@@ -49,15 +49,17 @@ function drawPaddle() {
 function drawBricks() {
     for(let c=0; c<brickColumnCount; c++) {
         for(let r=0; r<brickRowCount; r++) {
-            let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-            let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+            if(bricks[c][r].status == 1) {
+                let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
@@ -67,6 +69,7 @@ function draw() {
   drawBricks(); // Draws the brick wall that the ball hits
   drawBall(); // Shorten code inside draw();
   drawPaddle(); // Draws the paddle controlled by the user
+  collisionDetection(); // Creates detection by brick for the ball touching it
   x += dx;
   y += dy;
 
@@ -117,13 +120,17 @@ function keyUpHandler(e) { // function for assigning boolean values based on whe
 function collisionDetection() {
     for(let c=0; c<brickColumnCount; c++) {
         for(let r=0; r<brickRowCount; r++) {
-            let b = bricks[c][r]; // sets the variable b equal to the brick in column c and row r
-            if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-                dy = -dy;
+            let b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
             }
         }
     }
 }
 /**************************************************************/
+
 setInterval(draw, 10);
 window.onload = init;
